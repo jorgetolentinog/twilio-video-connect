476bfe924ee6297efe85ed2819fd0172b3b0b866
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-03-22 21:11:53 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import { TwilioMeeting } from "./component/TwilioMeeting/TwilioMeeting";
 
 export default class App extends Component {
   render() {
-    return (
-      <div>
-        <h1>Meeting</h1>
-        <TwilioMeeting />
-      </div>
-    );
+    return <TwilioMeeting />;
   }
 }
